44df14a08c74eeb098460863fcf35ef19052f21b
jon65 <xinquanyip@gmail.com>
2025-11-27 15:13:59 +1100

    create new portfolio website with more creative theming

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  // Enable standalone output for Docker
+  output: 'standalone',
 }
 
 module.exports = nextConfig
